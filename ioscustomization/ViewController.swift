8701503608c8884695c63965e8118950b33357ca
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-10-25 14:42:32 +0300

    dark mode finished.

--- ioscustomization/ViewController.swift
+++ ioscustomization/ViewController.swift
@@ -12,9 +12,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("hello")
+        
+        //in order to understand current preferences of the user
+          //we need uıinterfacestyle comes from traticollenction
+        
+        //To allow to only light mode in our app, see below
+        //it is valid only for this controller
+        overrideUserInterfaceStyle = .light
+        
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        }
+        else {
+            changeButton.tintColor = UIColor.blue
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        }
+        else {
+            changeButton.tintColor = UIColor.blue
+        }
+        
+        
+        
     }
 
-
+    @IBOutlet weak var changeButton: UIButton!
+    
 }
 
